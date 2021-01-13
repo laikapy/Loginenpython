1eb6de3f3a922c4dba0fbac678e016dfeba22427
Luis Angel J. M <petela.contacto@mail.com>
2021-01-12 19:38:16 -0600

    version dos listas

--- login.py
+++ login.py
@@ -1,3 +1,10 @@
+
+def claves(name):
+    databasekey = []
+    databasekey.append(name)
+    print(databasekey)
+
+
 def opciones(desicion):
     if desicion == 1:
         #inciar sesion
@@ -5,27 +12,26 @@ def opciones(desicion):
         login = input('Ingresa tu usuario: ')
         pasword = input('Ingresa tu contrasenia: ')
 
+
     elif desicion == 2:
         #registrarse
         print('Bienvenido a el registro.')
         name = input('Ingresa tu nombre completo')
+        databasekey = []
+        databasekey.append(name)
+        print(databasekey)
         paswordnew = input('Ingresa tu contrasenia nueva: ')
         paswordnew2 = input('Ingresa tu contrasenia de nuevo: ')
+        databasekey(name)
 
 
     else:
         print('Introduce una opcion correcta')
 
-#       Este es un cambio, solo para probar el gitsoft
-
-##Agregando esto nada mas xD
-
-
-este es un nuevo cambio, para probar el rm --cached
 
 def inicio():
     print('BIENVENIDO A LOGITEC')
-    print('1. Logearse \n 2.Iniciar sesion.')
+    print('1.Iniciar sesion.  \n 2.Logearse')
     desicion = int(input('Antes de comenzar, digite que es lo que quiere realizar: '))
     opciones(desicion)
 
